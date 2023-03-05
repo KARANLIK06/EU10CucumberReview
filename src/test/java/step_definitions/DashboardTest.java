b6a61611d0ab77e740b5ff29246af63b1588ab76
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-03-05 19:48:31 +0300

    week_04_03

---
+++ src/test/java/step_definitions/DashboardTest.java
@@ -0,0 +1,24 @@
+package step_definitions;
+
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import pages.DashboardPage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DashboardTest {
+    DashboardPage dashboardPage = new DashboardPage();
+
+    @Then("user should see menu option")
+    public void user_should_see_menu_options(List<String> expectedOptions) throws InterruptedException {
+        Thread.sleep(3000);
+        List<String> actualOptions = new ArrayList<>();
+        for (WebElement menu : dashboardPage.options) {
+            actualOptions.add(menu.getText());
+        }
+        Assert.assertEquals(expectedOptions,actualOptions);
+
+    }
+}
