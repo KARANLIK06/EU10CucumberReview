3c081d0254832455c961acd78a5038526aed47dc
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-03-05 19:48:10 +0300

    week_04_03

---
+++ src/test/java/runner/CukesRunner.java
@@ -0,0 +1,22 @@
+package runner;
+
+import io.cucumber.junit.*;
+import org.junit.runner.*;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+        },
+        features = "src/test/resources/features",
+        glue = "step_definitions",
+        dryRun = false,
+        tags = "@wip"
+)
+
+
+
+public class CukesRunner {
+
+}
+
