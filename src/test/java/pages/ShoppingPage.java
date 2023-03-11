260ca0001c9998bdc43204fe4761b347dfa8937b
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-03-11 23:10:26 +0300

    week_08

--- src/test/java/pages/ShoppingPage.java
+++ src/test/java/pages/ShoppingPage.java
@@ -5,10 +5,18 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.util.List;
+
 public class ShoppingPage {
     public ShoppingPage() {
         PageFactory.initElements(Driver.getDriver(),this);
     }
     @FindBy(css = "div.app_logo")
     public WebElement title;
+
+    @FindBy(xpath = "//span[@class='title']")
+    public WebElement subTitle;
+
+    @FindBy(css = "div.inventory_item")
+    public List<WebElement> items;
 }
